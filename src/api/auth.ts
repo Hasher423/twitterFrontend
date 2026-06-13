caa96a5bd5d5972f8a78047ddba5c473a984fa7a
HASHER <hasher423@gmail.com>
2026-06-13 06:33:32 +0500

    axios

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,5 @@
 // import axios from "axios";
 import axios from "axios";
-import { api } from "./axios";
 
 export const refreshAccessToken = async () => {
   const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/refreshToken`,{
